0eb53a6f7e977a75f936b451d3ebf4b395347bd7
Travis CI <travis@travis-ci.org>
2019-12-31 15:13:11 +0800

    update and test,

--- github-actions-merge/merge.py
+++ github-actions-merge/merge.py
@@ -10,20 +10,20 @@ from fabric.api import local, shell_env, lcd, run, settings
 
 BRANCH_TO_MERGE_INTO='develop'
 BRANCH_TO_MERGE_REGEX='^feature'
-TRAVIS_BRANCH = os.environ['GITHUB_REF']
-TRAVIS_COMMIT = os.environ['GITHUB_SHA']
-TRAVIS_BUILD_NUMBER = os.environ['GITHUB_ACTION']
-GITHUB_REPO = os.environ['GITHUB_REPOSITORY']
+GITHUB_REF = os.environ['GITHUB_REF']
+GITHUB_SHA = os.environ['GITHUB_SHA']
+GITHUB_ACTION = os.environ['GITHUB_ACTION']
+GITHUB_REPOSITORY = os.environ['GITHUB_REPOSITORY']
 # GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
 print("background information :")
 print({
   'BRANCH_TO_MERGE_INTO':'develop',
   'BRANCH_TO_MERGE_REGEX':'^refs/heads/feature',
-  'TRAVIS_BRANCH ': os.environ['GITHUB_REF'],
-  'TRAVIS_COMMIT ': os.environ['GITHUB_SHA'],
-  'TRAVIS_BUILD_NUMBER ': os.environ['GITHUB_ACTION'],
-  'GITHUB_REPO ': os.environ['GITHUB_REPOSITORY']
+  'GITHUB_REF ': os.environ['GITHUB_REF'],
+  'GITHUB_SHA ': os.environ['GITHUB_SHA'],
+  'GITHUB_ACTION ': os.environ['GITHUB_ACTION'],
+  'GITHUB_REPOSITORY ': os.environ['GITHUB_REPOSITORY']
 })
 
 def run_command(command_body):
@@ -31,23 +31,23 @@ def run_command(command_body):
   print(command_result)
   return command_result
 
-m = re.match(BRANCH_TO_MERGE_REGEX, TRAVIS_BRANCH)
+m = re.match(BRANCH_TO_MERGE_REGEX, GITHUB_REF)
 if (m == None ) :
-  print('skipping merge for branch {}'.format(TRAVIS_BRANCH))
-  print('skip merging for BUILD #{} `{}` from `{}` to `{}`'.format(TRAVIS_BUILD_NUMBER, GITHUB_REPO, TRAVIS_BRANCH, BRANCH_TO_MERGE_INTO), '#travis-build-result')
+  print('skipping merge for branch {}'.format(GITHUB_REF))
+  print('skip merging for BUILD #{} `{}` from `{}` to `{}`'.format(GITHUB_ACTION, GITHUB_REPOSITORY, GITHUB_REF, BRANCH_TO_MERGE_INTO), '#travis-build-result')
 
 else:
   with lcd(TEMP_DIR), settings(warn_only=True):
     print('checkout {} branch'.format(BRANCH_TO_MERGE_INTO))
     run_command('git checkout {}'.format(BRANCH_TO_MERGE_INTO))
 
-    print('Merging "{}"'.format(TRAVIS_COMMIT))
-    result_to_check = run_command('git merge --ff-only "{}"'.format(TRAVIS_COMMIT))
+    print('Merging "{}"'.format(GITHUB_SHA))
+    result_to_check = run_command('git merge --ff-only "{}"'.format(GITHUB_SHA))
 
     if result_to_check.failed:
-      print('error found during merging BUILD{} `{}` from `{}` to `{}`'.format(TRAVIS_BUILD_NUMBER, GITHUB_REPO, TRAVIS_BRANCH, BRANCH_TO_MERGE_INTO), '#travis-build-result')
+      print('error found during merging BUILD{} `{}` from `{}` to `{}`'.format(GITHUB_ACTION, GITHUB_REPOSITORY, GITHUB_REF, BRANCH_TO_MERGE_INTO), '#travis-build-result')
     else:
-      print('merging BUILD{} from {} `{}` to `{}` done'.format(TRAVIS_BUILD_NUMBER, GITHUB_REPO, TRAVIS_BRANCH, BRANCH_TO_MERGE_INTO), '#travis-build-result')
+      print('merging BUILD{} from {} `{}` to `{}` done'.format(GITHUB_ACTION, GITHUB_REPOSITORY, GITHUB_REF, BRANCH_TO_MERGE_INTO), '#travis-build-result')
 
     print('push commit')
     run_command("git push {} {}".format(PUSH_URI, BRANCH_TO_MERGE_INTO))

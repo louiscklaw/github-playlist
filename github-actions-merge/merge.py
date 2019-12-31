fd4ff74c2a6adc1f490c14d1c24eca69ccd4594a
Travis CI <travis@travis-ci.org>
2019-12-31 15:22:01 +0800

    update and test,

--- github-actions-merge/merge.py
+++ github-actions-merge/merge.py
@@ -15,7 +15,7 @@ GITHUB_SHA = os.environ['GITHUB_SHA']
 GITHUB_ACTION = os.environ['GITHUB_ACTION']
 GITHUB_REPOSITORY = os.environ['GITHUB_REPOSITORY']
 # GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
-PUSH_URI=f"https://github.com/louiscklaw/{GITHUB_REPO}.git"
+PUSH_URI=f"https://github.com/louiscklaw/{GITHUB_REPOSITORY}.git"
 
 TEMP_DIR = local('mktemp -d', capture=True)
 local('git clone "{}" "{}"'.format(PUSH_URI, TEMP_DIR))

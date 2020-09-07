9c894a238f71074d43520d9472d6a5bb57801399
louiscklaw <louiscklaw@gmail.com>
2020-09-07 19:34:03 +0800

    ci: update and test,

--- github-build-merger/env_config.py
+++ github-build-merger/env_config.py
@@ -6,7 +6,9 @@ import chalk
 from fabric.api import local, shell_env, lcd, run, settings
 
 TRAVIS_BRANCH=os.environ['GITHUB_REF']
-GITHUB_REPO = 'louisckalw/github-playlist'
+GITHUB_REF=os.environ['GITHUB_REF']
+
+GITHUB_REPO = os.environ['GITHUB_REPOSITORY']
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
 PUSH_URI="https://{}@github.com/{}".format(GITHUB_TOKEN, GITHUB_REPO)

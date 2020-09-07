0ed984df8a67f51e29e9b1ffdf1014eeb00d1c5c
louiscklaw <louiscklaw@gmail.com>
2020-09-07 17:37:29 +0800

    ci: update and test,

---
+++ github-build-merger/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# reference build https://travis-ci.org/louiscklaw/test_git_repo/builds/625335510
+# https://docs.travis-ci.com/user/environment-variables/
+
+import sys
+import os, re, subprocess
+import slack
+import chalk
+
+from fabric.api import local, shell_env, lcd, run, settings
+
+
+def main():
+  console.log('helloworld')
+
+
+
+if __name__ == "__main__":
+  main()

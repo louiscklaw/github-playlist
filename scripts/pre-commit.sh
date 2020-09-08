649e24b9259fe7d542cb68711945dba032a15e10
louiscklaw <louiscklaw@gmail.com>
2020-09-09 00:17:58 +0800

    update and test,

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,7 +5,5 @@ set -e
 echo 'updating master_build.yml...'
 ./update_main_build_chain.sh
 
-sleep 1
-
 echo 'adding master_build.yml into commit...'
 git add /home/logic/_workspace/github-playlist/.github/workflows/master_build.yml

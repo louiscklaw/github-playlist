5fb3417dd51ba0bf7b87f19fa1f2a17c1fef7f58
louiscklaw <louiscklaw@gmail.com>
2020-09-20 23:57:09 +0800

    update and test,

--- github-actions-firebase-tryout/test.sh
+++ github-actions-firebase-tryout/test.sh
@@ -2,4 +2,6 @@
 
 set -ex
 
-rsync -avzh --progress github-actions-helloworld-tryout/github-actions-helloworld-tryout.yml .github/workflows
+# rsync -avzh --progress github-actions-helloworld-tryout/github-actions-helloworld-tryout.yml .github/workflows
+
+curl -Lo ./firebase_bin https://firebase.tools/bin/linux/latest

fae2e3826e570c3adc46d42b59a577e1cba03986
louiscklaw <louiscklaw@gmail.com>
2020-09-22 20:18:07 +0800

    update and test,

--- build-dashboard-tryout/src/pages/index.js
+++ build-dashboard-tryout/src/pages/index.js
@@ -4,16 +4,15 @@ import Navbar from '../components/nav'
 
 import SEO from "../components/seo"
 
-import ThemeContext from '../contexts/ThemeContext'
+import RepoStatusContainer from '../components/RepoStatusContainer'
 
 
 function IndexPage(){
   return(
     <Layout>
-
       <SEO title="Home" />
       <Navbar />
-
+      <RepoStatusContainer />
     </Layout>
   )
 }

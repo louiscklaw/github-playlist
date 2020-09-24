7962c575393fe357961f81a99a4cea0140816fc1
louiscklaw <louiscklaw@gmail.com>
2020-09-24 16:16:47 +0800

    working commit,

--- build-dashboard-tryout/gatsby-config.js
+++ build-dashboard-tryout/gatsby-config.js
@@ -6,6 +6,31 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-  pathPrefix: `/github-playlist`
+  pathPrefix: `/github-playlist`,
+  plugins: [
+    `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
 }

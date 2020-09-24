aa1ca3f920febeac1fba268d8840e4f731164ed8
louiscklaw <louiscklaw@gmail.com>
2020-09-24 16:42:52 +0800

    working commit,

--- build-dashboard-tryout/src/components/nav/index.js
+++ build-dashboard-tryout/src/components/nav/index.js
@@ -5,6 +5,20 @@ import ThemeContext from '../../contexts/ThemeContext'
 import {combineStyles} from '../../utils/common'
 
 function Navbar(props){
+  const {active_style} = React.useContext(ThemeContext)
+
+  const checkActivePage = (page_href) =>{
+    if (typeof window !== `undefined`){
+      var temp = '.+'+page_href+'$'
+      var re = new RegExp(temp,'g')
+      if (window.location.href.search(re) == 0){
+        return active_style.isActive
+      }else{
+        return ''
+      }
+    }
+  }
+
   return(
     <>
     </>
--- build-dashboard-tryout/src/pages/index.js
+++ build-dashboard-tryout/src/pages/index.js
@@ -1,6 +1,11 @@
 import React from "react"
 import { navigate } from "gatsby"
 import Layout from "../components/layout"
+import Navbar from '../components/nav'
+
+import SEO from "../components/seo"
+
+import RepoStatusContainer from '../components/RepoStatusContainer'
 
 export default function Home() {
   return <div>Hello world!</div>

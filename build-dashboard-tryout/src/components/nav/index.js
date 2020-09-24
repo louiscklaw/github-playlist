258f798d3f8c0c921285676612d06add533a622f
louiscklaw <louiscklaw@gmail.com>
2020-09-24 16:28:04 +0800

    working commit,

---
+++ build-dashboard-tryout/src/components/nav/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'gatsby'
+
+import ThemeContext from '../../contexts/ThemeContext'
+import {combineStyles} from '../../utils/common'
+
+function Navbar(props){
+  return(
+    <>
+    </>
+  )
+}
+
+export default Navbar
--- build-dashboard-tryout/src/pages/index.js
+++ build-dashboard-tryout/src/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { navigate } from "gatsby"
+import Layout from "../components/layout"
 
 export default function Home() {
   return <div>Hello world!</div>

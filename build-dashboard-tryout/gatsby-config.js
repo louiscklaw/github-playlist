59fa53b3fb255ea93343f718aa93e24be13658c5
louiscklaw <louiscklaw@gmail.com>
2020-09-25 00:04:33 +0800

    update and test,

--- build-dashboard-tryout/gatsby-config.js
+++ build-dashboard-tryout/gatsby-config.js
@@ -5,6 +5,8 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,

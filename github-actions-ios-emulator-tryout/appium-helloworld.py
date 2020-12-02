06301b75f04f2e7217f620d0bb138113f19e71f9
louiscklaw <louiscklaw@gmail.com>
2020-12-02 11:57:17 +0800

    update and test for ios devices,

--- github-actions-ios-emulator-tryout/appium-helloworld.py
+++ github-actions-ios-emulator-tryout/appium-helloworld.py
@@ -2,6 +2,9 @@
 import unittest
 from appium import webdriver
 
+CURR_DIR=os.path.abspath(os.path.dirname(__file__))
+SCREEN_CAPTURE_DIR='{}/screens'.format(CURR_DIR)
+
 desired_caps = dict(
     platformName='iOS',
     platformVersion='11.4',
@@ -11,6 +14,20 @@ desired_caps = dict(
     browserName='safari'
 )
 
-self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-el = self.driver.find_element_by_accessibility_id('item')
-el.click()
+def getScreenShot(driver, sc_filename):
+  img_data = driver.get_screenshot_as_base64()
+  with open(sc_filename, "wb") as fh:
+    fh.write(base64.urlsafe_b64decode(img_data))
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+driver.implicitly_wait(30)
+
+
+getScreenShot(driver, '{}/hello_chrome.png'.format(SCREEN_CAPTURE_DIR))
+
+# el = driver.find_element_by_accessibility_id('item')
+# el.click()
+
+driver.quit()
+
+print('done')
